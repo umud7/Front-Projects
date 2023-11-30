6b9c69241d9e534dc764780267f1b8c103ea94a8
umud <umidhg@code.edu.az>
2023-12-01 00:48:59 +0400

    Restaurant

---
+++ Vebsite/assets/Js/script.js
@@ -0,0 +1,45 @@
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active');
+    searchForm.classList.remove('active');
+}
+
+let searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () =>{
+    searchForm.classList.toggle('active');
+    navbar.classList.remove('active');
+}
+
+// Top Icons JS Code
+
+$(document).ready(function() {
+    $('.glow').mouseenter(function() {
+      $(this).addClass('glow-effect');
+    });
+    $('.glow').mouseleave(function() {
+      $(this).removeClass('glow-effect');
+    });
+  });
+
+
+    // Start Fixed Section Home Page
+const counts = document.querySelectorAll('.count')
+const speed = 15
+
+counts.forEach((counter) => {
+    function upDate(){
+        const target = Number(counter.getAttribute('data-target'))
+        const count = Number(counter.innerText)
+        const inc = target / speed        
+        if(count < target){
+            counter.innerText = Math.floor(inc + count) 
+            setTimeout(upDate, 60)
+        }else{
+            counter.innerText = target
+        }
+    }
+    upDate()
+})
+// End Fixed Section Home Page
